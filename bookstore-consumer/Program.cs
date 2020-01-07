dbe4564f66916c479c49316e6c504e6c5cffcfd7
mischameyer <mischa@meyer74.ch>
2020-01-07 15:16:52 +0100

    first running version

---
+++ bookstore-consumer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace bookstore_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            ConsumerConfig consumerConfig = null;
+            
+            CreateConfig();
+            CreateConsumerAndConsume();
+            
+
+            void CreateConfig()
+            {
+                consumerConfig = new ConsumerConfig
+                {
+                    BootstrapServers = "localhost:9092",
+                    GroupId = "test-group",
+                    AutoOffsetReset = AutoOffsetReset.Earliest
+                };
+            }
+
+            void CreateConsumerAndConsume()
+            {
+
+                var cb = new ConsumerBuilder<string, string>(consumerConfig);
+
+                Console.WriteLine("Press Ctrl+C to exit");
+
+                Console.CancelKeyPress += new ConsoleCancelEventHandler(OnExit);
+
+                using (var consumer = cb.Build())
+                {
+                    consumer.Subscribe("testTopic");
+
+                    try
+                    {
+                        while (!cts.IsCancellationRequested)
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            var offset = cr.TopicPartitionOffset;
+                            Console.WriteLine($"Message '{cr.Value}' at: '{offset}'.");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        consumer.Close();
+                    }
+                }
+            }
+
+            void OnExit(object sender, ConsoleCancelEventArgs args)
+            {
+                args.Cancel = true;
+                Console.WriteLine("In OnExit");
+                cts.Cancel();
+
+            }
+
+        }
+    }
+}
---
+++ bookstore-producer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace bookstore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoResetEvent _closing = new AutoResetEvent(false);
+
+            IProducer<string, string> producer = null;
+            ProducerConfig producerConfig = null;
+
+            CreateConfig();
+            CreateProducer();
+            SendMessage("testTopic", "This is a test42");
+            Console.WriteLine("Press Ctrl+C to exit");
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(OnExit);
+            _closing.WaitOne();
+
+
+            void OnExit(object sender, ConsoleCancelEventArgs args)
+            {
+                Console.WriteLine("Exit");
+                _closing.Set();
+            }
+
+            void CreateConfig()
+            {
+                producerConfig = new ProducerConfig
+                {
+                    BootstrapServers = "localhost:9092"
+                };
+            }
+
+            void CreateProducer()
+            {
+                var pb = new ProducerBuilder<string, string>(producerConfig);
+                producer = pb.Build();
+            }
+
+            async void SendMessage(string topic, string message)
+            {
+                var msg = new Message<string, string>
+                {
+                    Key = null,
+                    Value = message
+                };
+
+                var delRep = await producer.ProduceAsync(topic, msg);
+                var topicOffset = delRep.TopicPartitionOffset;
+
+                Console.WriteLine($"Delivered '{delRep.Value}' to: {topicOffset}");
+            }
+
+        }
+    }
+}
